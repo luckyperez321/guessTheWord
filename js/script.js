89a9c8254e0fd0be129c5da5d7f2b83c384b5870
Lucky Perez <luckyperez321@gmail.com>
2024-05-01 09:42:06 -0500

    accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ const messageWall = document.querySelector(".message");
 const playAgain = document.querySelector(".play-again");
 /*The hidden button that will appear prompting the player to play again.*/
 const word = "magnolia";
+const guessedLetters = [];
+
 
 const circles = function (word) {
     const circleLetters = [];
@@ -27,9 +29,63 @@ const circles = function (word) {
 
 circles(word);
 
+
+
+
+
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    messageWall.innerText = "";
+
+
     const inputValue = guessForm.value;
     console.log(inputValue);
     guessForm.value = "";
+    playerInput(inputValue);
+
+    const result = playerInput(inputValue);
+    console.log(result);
 });
+/*Feel like this whole guestButton function could be cleaned up and has extra stuff in it*/
+
+
+
+
+
+/* Accept and Validate Player Guesses*/
+const playerInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === '') {
+        console.log("Lets Play! Enter a letter");
+    }
+    else if (input.length > 1) {
+        console.log("Only one letter at a time, cowboy");
+    }
+    else if (!input.match(acceptedLetter)) {
+        messageWall.innerText = "Please Enter A Letter From A-Z";
+    }
+    else {
+        return input();
+    }
+};
+
+const makeGuess = function (guessButton) {
+    guessButton = guessButton.toUpperCase();
+    if (guessedLetters.includes(guessButton)) {
+        messageWall.innerText = "That Letter Has Already Been Guessed";
+    }
+    else {
+        guessedLetters.push(guessButton);
+
+        console.log(guessedLetters);
+    }
+};
+
+/*
+What I had orginally for lines 67-68. The last else statement in the playerInput function
+else (input.match(/[a-zA-Z]/))
+{
+console.log ("Great One!")  
+*/
+
+
